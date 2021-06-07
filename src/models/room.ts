acfd9f495294e089e3ec83f8cf3c738a2aa0b3d1
AnInternetTroll <lucafulger@gmail.com>
2021-06-07 18:51:11 +0100

    Add subscription and invite users feature

--- src/models/room.ts
+++ src/models/room.ts
@@ -2,7 +2,8 @@ import { Schema, model, Document } from "mongoose";
 import { composeWithMongoose } from "graphql-compose-mongoose";
 import { Id } from "./common";
 import { ExpressContext } from "apollo-server-express";
-import { UserTC } from "./user";
+import { UserInputError } from "apollo-server-errors";
+import { User, UserTC } from "./user";
 
 export interface IRoom extends Document {
 	id: string;
@@ -101,6 +102,44 @@ RoomTC.addResolver({
 			...args,
 		});
 		await room.save();
+		// @ts-ignore If the statement above doesn't throw an error then there will definetly be a room with this ID
+		return await Room.findById(room.id).exec();
+	},
+});
+
+RoomTC.addResolver({
+	type: RoomTC,
+	name: "addMemberToRoom",
+	description: "Add users by name to one of your rooms.",
+	args: {
+		members: "[String]!",
+		room: "String!",
+	},
+	resolve: async ({
+		context,
+		args,
+	}: {
+		context: ExpressContext;
+		args: any;
+	}): Promise<IRoom> => {
+		const room = await Room.findOne({
+			_id: args.room,
+			members: context.user?.id,
+		}).exec();
+		if (!room) throw new UserInputError("Room not found.");
+		if (room.owner !== context.user?.id)
+			throw new UserInputError("You are not the owner of this room.");
+		const members = await User.find({ username: args.members });
+		if (!members.length) throw new UserInputError("No users found.");
+		members.forEach((el) => {
+			if (room.members.includes(el.id))
+				throw new UserInputError("Member already in the room.");
+		});
+		await Room.findByIdAndUpdate(args.room, {
+			$push: {
+				members: members.map((el) => el.id),
+			},
+		});
 		// @ts-ignore If the statement above doesn't throw an error then there will definetly be a room with this ID
 		return await Room.findById(room.id).exec();
 	},
